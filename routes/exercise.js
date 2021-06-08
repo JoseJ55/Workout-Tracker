0993d48806e54c6c15d74fd01719b155e1586492
Jose Jimenez <josejimenez826@gmail.com>
2021-06-08 14:57:47 -0700

    finally got all of front end working

--- routes/exercise.js
+++ routes/exercise.js
@@ -1,9 +1,21 @@
 const router = require('express').Router();
 
+const Workout = require('../models/Workout');
+
 router.get('/', (req, res) => {
     try{
         // res.send("working");
-        res.render("exercise")
+        const workout = Workout.find({}, (err, result) => {
+            if (err) {
+                console.log(err);
+              } else {
+                // res.json(result);
+                console.log(result);
+                res.render("exercise");
+            }
+        });
+        // console.log(workout)
+        
     } catch (err) {
         res.status(500).json(err);
     }
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const path = require('path');
 const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({});
-// const Workout = require('./models/Workout');
+const Workout = require('./models/Workout');
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {useNewUrlParsertrue})
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {useNewUrlParser: true})
 
 app.use(routes)
 
