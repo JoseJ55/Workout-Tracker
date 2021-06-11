44201ab743e814ab439c0b0a61b22cd465beaee7
Jose Jimenez <josejimenez826@gmail.com>
2021-06-11 16:33:23 -0700

    fixing last few heroku issues

--- routes/api/stats.js
+++ routes/api/stats.js
@@ -3,9 +3,9 @@ const Workout = require('../../models/Workout');
 
 router.get('/workouts', async (req, res) => {
     try{
-        const workout = await Workout.find({read:true}, (err, found))
-        res.send(workout);
-        // res.render("stats")
+        // const workout = await Workout.find({read:true}, (err, found))
+        // res.send(workout);
+        res.render("stats")
     } catch(err) {
         res.status(500).json(err);
     }
